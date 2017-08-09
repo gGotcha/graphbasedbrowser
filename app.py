13ccccfbb856d26e975b4f700b8c1a92e67698c8
Your Name <you@example.com>
2017-08-09 19:20:12 +0300

    tt

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-	return "Yo, its working!"
+	return render_template('index.html')
 if __name__ == "__main__":
 	app.run()
